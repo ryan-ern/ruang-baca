9224a329821bc9caa341078b4e2b801ad00797ec
ryan-ern <ryanernanda100902@gmail.com>
2023-11-18 20:57:34 +0700

    update bug saga

--- Frontend/src/store/sagas.jsx
+++ Frontend/src/store/sagas.jsx
@@ -1,12 +1,12 @@
 import { all, fork } from 'redux-saga/effects';
 import authSaga from './auth/saga';
 import dashboardSaga from './dashboard/saga';
-import inventoryReducer from './inventory/reducer';
+import inventorySaga from './inventory/saga';
 
 export default function* rootSaga() {
     yield all([
         fork(authSaga),
         fork(dashboardSaga),
-        fork(inventoryReducer)
+        fork(inventorySaga)
     ]);
 }
